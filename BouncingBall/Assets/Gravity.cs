3be28c6f3ec7a70bca9924af47327caa9aec195f
Jamie Fraser <jdf4@hw.ac.uk>
2018-02-06 20:18:09 +0000

    clean up

--- BouncingBall/Assets/Gravity.cs
+++ BouncingBall/Assets/Gravity.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour {
 
-
-
-	Vector3 velocity = new Vector3(0, 0, 0);
-	Vector3 acceleration;
-	Vector3 rotation;
-	Vector3 gravity = new Vector3(0, -9.8f, 0);
-	Vector3 force;
-	float mass = 20.0f;
-	float maxSpeed = 1.0f;
-	bool moving = true;
-	bool decent = true;
-	float engeryLoss = 0.8f;
+	//objects floor and sphere col;ider
 	GameObject floor;
 	GameObject Collider;
+
+	//physics and movemnt
+	Vector3 velocity = new Vector3(0, 0, 0);
+	Vector3 gravity = new Vector3(0, -9.8f, 0);
+	float engeryLoss = 0.8f;
+
+	
+	bool moving;
+	bool decent;
 	int bounce = 0;
+
+
+
+
+
 	// Use this for initialization
 	void Start () 
 	{
-		force = gravity * mass;
-		moving = true;
+
 		floor =  GameObject.Find("Cube");
 		Collider = GameObject.Find("sphereCollider");
+
+		moving = true;
+		decent = true;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
@@ -36,21 +40,27 @@ public class Gravity : MonoBehaviour {
 
 	void FixedUpdate(){
 		
-		
-		//Debug.Log(velocity.y);
+		//if ball is in motion
 		if(moving){
 
+
+			//ensures the ball only bounces on decent
 			if(velocity.y <= 0){
 				decent = true;
 			}
 			else
 				decent = false;
 			
-				
+
+			//if ball hits the floor cube	
 			if((Collider.transform.position.y-2.5) <= (floor.transform.position.y) && decent){
 				bounce++;
 				Debug.Log(bounce);
+
+				//engery is lost(realisticly to sound and heat)
 				velocity.y = (velocity.y * -1) * engeryLoss;
+
+				//ball stopes bouncing
 				if(bounce == 20){
 					moving = false;
 				}
@@ -58,15 +68,12 @@ public class Gravity : MonoBehaviour {
 				
 			}
 
-
-
+			//acceleration and deceleration due to grabity
 			velocity = velocity + gravity * Time.fixedDeltaTime;
-				
-			//Debug.Log("ball :"+ velocity);
+			//ball and colider movement based on new velocity
 			transform.Translate(velocity * Time.fixedDeltaTime);
-			//Debug.Log("colider :"+ velocity);
-			Collider.transform.position =transform.position;
-			//Debug.Log(velocity);
+			Collider.transform.position = transform.position;
+			
 		}
 
 		
